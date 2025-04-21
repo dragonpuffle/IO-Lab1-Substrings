b47dd9fe577a3fc6f328c0f84e4ba109e0a18a95
X-D-R <dabholak@gmail.com>
2025-04-21 16:06:39 +0300

    Naive algorithm simplified + Boyer-Moore-Horspool algorithm

--- main.py
+++ main.py
@@ -20,13 +20,53 @@ def naive_algorithm(substring: str, main_text: str) -> int:
         if substring == main_text[i:i + substr_len]:
             return i
 
+def make_bias_table(substring: str)->dict:
+    substring_len = len(substring)
+    bias_table = {"END": substring_len}
+    for i in range(1, substring_len):
+        if substring[substring_len - 1 - i] not in bias_table.keys():
+            bias_table[substring[substring_len - 1 - i]] = i
+    if substring[substring_len - 1] not in bias_table.keys():
+        bias_table[substring[substring_len - 1]] = substring_len
+    return bias_table
+
+def Boyer_Moore_Horspool_algorithm(substring: str, main_text: str) -> int:
+    substring_len = len(substring)
+    main_text_len = len(main_text)
+    bias_table = make_bias_table(substring)
+    index = 0
+    while index + substring_len < main_text_len:
+        flag = True
+        for i in range(substring_len):
+            if substring[substring_len - 1 - i] != main_text[index + substring_len - 1 - i]:
+                if i == 0:
+                    if main_text[index + substring_len - 1 - i] in bias_table.keys():
+                        index += bias_table[main_text[index + substring_len - 1 - i]]
+                    else:
+                        index += bias_table["END"]
+                else:
+                    index += bias_table[substring[substring_len - 1]]
+                flag = False
+                break
+        if flag:
+            return index
+    return -1
 
 if __name__ == "__main__":
-    # Naive algorithm
+    # Substring and text
     substring = read_file("substr.txt")
     text = read_file("test.txt")
-    print(f"Substring: {substring}\nText: {text}\n")
+    print(f"Substring: {substring}\nText: {text}")
+    # Naive algorithm
+    print("\n-----Naive algorithm-----")
     pos = naive_algorithm(substring, text)
+    print(f"Position of substring: {pos}")
+    if pos > 0:
+        print(f"\nText started with substring: {text[pos:]}")
+
+    # Boyer-Moore-Horspool algorithm
+    print("\n-----Boyer-Moore-Horspool algorithm-----")
+    pos = Boyer_Moore_Horspool_algorithm(substring, text)
     print(f"Position of substring: {pos}")
     if pos > 0:
         print(f"\nText started with substring: {text[pos:]}")
